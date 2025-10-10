1ed59dc9f534a8aefbb0655ed47f0b045c1bfa0a
Eloooooo03 <eloiza.silva01@escola.pr.gov.br>
2025-10-10 08:03:22 -0300

    oo

---
+++ perguntas.js
@@ -0,0 +1,14 @@
+Criacartao(
+    'programacao'
+    'oque e python?'
+    'python e uma lingugagem de programaçao'
+)
+Criacartao(
+    'geografia'
+    'qual e a capital da frança'
+    'capital da frança e paris'
+)
+Criacartao(
+    'programaçao'
+    'oque e uma funcao'
+   'uma função é um  bloco que executa alguma tarefa')
