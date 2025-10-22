6f0c39d354f99c26a0bda9c6ae9ab2f51f11ef90
Eloooooo03 <eloiza.silva01@escola.pr.gov.br>
2025-10-22 08:49:48 -0300

    oooo

---
+++ app.js
@@ -0,0 +1,6 @@
+function criacartao(cartegoria,pergunta,resposta){
+    let container=
+    document.getElementById('container')
+    let cartao =document.createElement('article')
+    cartao.classname='cartao'
+}
