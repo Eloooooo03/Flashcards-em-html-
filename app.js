cc88bdab17ca00c6204f0b511395eebcf8838fd7
Eloooooo03 <eloiza.silva01@escola.pr.gov.br>
2025-11-12 07:58:37 -0300

    o

--- app.js
+++ app.js
@@ -21,9 +21,7 @@ function criaCartao(categoria, pergunta, resposta) {
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active',respostaEstaVisivel)
     }
-    cartao.addEventListener('click', viraCartao)
-
-
+    cartao.addEventListener('click', viraCartao)o
     container.appendChild(cartao)
 
 }
