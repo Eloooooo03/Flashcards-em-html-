d4f12aae7598d930127142f4475fa79409fbd990
Eloooooo03 <eloiza.silva01@escola.pr.gov.br>
2025-11-19 08:05:50 -0300

    ;-;

--- app.js
+++ app.js
@@ -1,40 +1,31 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-    let conteudo = document.createElement('div');
-    conteudo.className = 'cartao__conteudo';
-    
-    let h3 = document.createElement('h3');
-    h3.textContent = categoria;
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
 
-    let perguntaDiv = document.createElement('div');
-    perguntaDiv.className = 'cartao__conteudo__pergunta';
-    let perguntaP = document.createElement('p');
-    perguntaP.textContent = pergunta;
-    perguntaDiv.appendChild(perguntaP);
-
-    let respostaDiv = document.createElement('div');
-    respostaDiv.className = 'cartao__conteudo__resposta';
-    let respostaP = document.createElement('p');
-    respostaP.textContent = resposta;
-    respostaDiv.appendChild(respostaP);
-
-    conteudo.appendChild(h3);
-    conteudo.appendChild(perguntaDiv);
-    conteudo.appendChild(respostaDiv);
-
-    cartao.appendChild(conteudo);
-
-    let respostaEstaVisivel = false;
+    let respostaEstaVisivel = false
 
     function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active', respostaEstaVisivel);
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
+    cartao.addEventListener('click', viraCartao)
 
-    cartao.addEventListener('click', viraCartao);
 
-    container.appendChild(cartao);
+    container.appendChild(cartao)
+
 }
